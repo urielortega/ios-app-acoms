ee7fb0e90b19a185d79035e2a03d10a2b1d9deb0
Uriel Ortega <urielortega2509@gmail.com>
2023-07-31 12:32:28 -0600

    Initial Commit

---
+++ AppACOMs/AppACOMsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppACOMsApp.swift
+//  AppACOMs
+//
+//  Created by Uriel Ortega on 31/07/23.
+//
+
+import SwiftUI
+
+@main
+struct AppACOMsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
